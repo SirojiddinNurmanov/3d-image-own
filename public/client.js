3c5786b3175e30032e2aa11921eaeb225e2254e1
SirojiddinNurmanov <oltinasr1203@gmail.com>
2022-09-03 18:07:16 +0500

    second commit

--- public/client.js
+++ public/client.js
@@ -1,10 +1,10 @@
-import * as THREE from '/build/three.module.js';
-import Stats from './jsm/libs/stats.module.js';
-import { OrbitControls } from './jsm/controls/OrbitControls.js';
+import * as THREE from "/build/three.module.js";
+import Stats from "./jsm/libs/stats.module.js";
+import { OrbitControls } from "./jsm/controls/OrbitControls.js";
 console.log(THREE);
 console.log(OrbitControls);
 
-const canvas = document.querySelector('.web-gl');
+const canvas = document.querySelector(".web-gl");
 
 // showing fps
 const stats = new Stats();
@@ -27,12 +27,12 @@ console.log(camera);
 
 // Render Setup
 const renderer = new THREE.WebGLRenderer({
-    antialias: true,
-    canvas: canvas,
+  antialias: true,
+  canvas: canvas,
 });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio((window.devicePixelRatio) ? window.devicePixelRatio : 1);
+renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1);
 renderer.autoClear = false;
 renderer.setClearColor = (0x000000, 0.0);
 console.log(renderer);
@@ -50,22 +50,22 @@ let loader = new THREE.TextureLoader();
 let textureArray = [];
 
 // all texture
-let frontTexture = loader.load('./model/polluted_earth/front.jpg');
-let backTexture = loader.load('./model/polluted_earth/back.jpg');
-let topTexture = loader.load('./model/polluted_earth/top.jpg');
-let bottomTexture = loader.load('./model/polluted_earth/bottom.jpg');
-let rightTexture = loader.load('./model/polluted_earth/left.jpg');
-let leftTexture = loader.load('./model/polluted_earth/right.jpg');
+let frontTexture = loader.load("./model/my_room/front.jpg");
+let backTexture = loader.load("./model/my_room/back.jpg");
+let topTexture = loader.load("./model/my_room/top.jpg");
+let bottomTexture = loader.load("./model/my_room/bottom.jpg");
+let rightTexture = loader.load("./model/my_room/left.jpg");
+let leftTexture = loader.load("./model/my_room/right.jpg");
 
-textureArray.push(new THREE.MeshBasicMaterial({map: frontTexture}));
-textureArray.push(new THREE.MeshBasicMaterial({map: backTexture}));
-textureArray.push(new THREE.MeshBasicMaterial({map: topTexture}));
-textureArray.push(new THREE.MeshBasicMaterial({map: bottomTexture}));
-textureArray.push(new THREE.MeshBasicMaterial({map: rightTexture}));
-textureArray.push(new THREE.MeshBasicMaterial({map: leftTexture}));
+textureArray.push(new THREE.MeshBasicMaterial({ map: frontTexture }));
+textureArray.push(new THREE.MeshBasicMaterial({ map: backTexture }));
+textureArray.push(new THREE.MeshBasicMaterial({ map: topTexture }));
+textureArray.push(new THREE.MeshBasicMaterial({ map: bottomTexture }));
+textureArray.push(new THREE.MeshBasicMaterial({ map: rightTexture }));
+textureArray.push(new THREE.MeshBasicMaterial({ map: leftTexture }));
 
-for(let i=0; i<textureArray.length; i++){
-    textureArray[i].side = THREE.BackSide;
+for (let i = 0; i < textureArray.length; i++) {
+  textureArray[i].side = THREE.BackSide;
 }
 
 // making cube
@@ -74,24 +74,24 @@ const skyBox = new THREE.Mesh(cubeGeometry, textureArray);
 scene.add(skyBox);
 
 // render function to render the scene
-const render = ()=>{
-    renderer.render(scene, camera);
-}
+const render = () => {
+  renderer.render(scene, camera);
+};
 
 // Recursion function for animation
-const animate = ()=>{
-    requestAnimationFrame(animate);
-    render();
-    stats.update();
-}
+const animate = () => {
+  requestAnimationFrame(animate);
+  render();
+  stats.update();
+};
 animate();
 
 // Resizing window to make responsive
-const windowResize = ()=>{
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    render();
-}
+const windowResize = () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  render();
+};
 
-window.addEventListener('resize', windowResize, false);
+window.addEventListener("resize", windowResize, false);
